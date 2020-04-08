b9da3faff9ad9bc7143fd9fc3a38a690d96c2afd
adi76 <maciej.adrjanowicz@wp.pl>
2020-04-08 11:59:26 +0200

    123

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Twig\Environment;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/question/{question}", name="question")
+     */
+    public function question($question)
+    {
+        dump($this);
+        return $this->render('question/index.html.twig', [
+            'question' => $question
+        ]);
+    }
+}
